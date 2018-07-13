ffbef3439730192c3aa5e06ba793e9b12fba745e
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-13 08:39:25 -0700

    add properties for both stylist and client classes

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using HairSalon;
+using System;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+
+        public Client(int stylistId, string name, string phone, string email, int id = 0)
+        {
+            StylistId = stylistId;
+            Name = name;
+            Phone = phone;
+            Email = email;
+            Id = id;
+        }
+    }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using HairSalon;
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        public int StylistId { get; set; }
+        public string StylistName { get; set; }
+    }
+        public Stylist()
+        {
+            
+        }
+    }
+}
