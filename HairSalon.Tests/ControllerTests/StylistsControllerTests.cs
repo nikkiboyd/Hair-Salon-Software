e2c80da015ea21849e6a371006514f48ec556402
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-13 15:40:15 -0700

    write and pass tests for stylistscontroller

---
+++ HairSalon.Tests/ControllerTests/StylistsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests.ControllerTests
+{
+    [TestClass]
+    public class StylistsControllerTests
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult indexView = controller.Index();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Create_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult indexView = controller.Create();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Create_HasCorrectModelType_True()
+        {
+            ViewResult indexView = new StylistsController().Create() as ViewResult;
+            var result = indexView.ViewData.Model;
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+    }
+}
