154926c65cbd1478188c3fb9b82656cc72f9dd07
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-13 09:57:01 -0700

    write methods for Equals and DeleteAll and pass tests

---
+++ HairSalon.Tests/ModelsTests/StylistTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+using System.Collections.Generic;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class StylistTests : IDisposable
+    {
+        public StylistTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=nikki_boyd_test;";
+        }
+
+        public void Dispose()
+        {
+            Stylist.DeleteAll();
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueIfStylitsAreTheSame_True()
+        {
+            Stylist firstStylist = new Stylist(1, "test name");
+            Stylist secondStylist = new Stylist(1, "test name");
+
+            Assert.AreEqual(firstStylist, secondStylist);
+        }
+    }
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -10,10 +10,42 @@ namespace HairSalon.Models
         public int StylistId { get; set; }
         public string StylistName { get; set; }
 
-        public Stylist(string name, int stylistId)
+        public Stylist(int stylistId, string stylistName)
         {
-            StylistName = name;
+            StylistName = stylistName;
             StylistId = stylistId;
+        }
+
+        public override bool Equals(System.Object otherStylist)
+        {
+            if (!(otherStylist is Stylist))
+            {
+                return false;
+            }
+            else
+            {
+                Stylist newStylist = (Stylist)otherStylist;
+                bool stylistIdEquality = (this.StylistId == newStylist.StylistId);
+                bool nameEquality = (this.StylistName == newStylist.StylistName);
+
+                return (stylistIdEquality && nameEquality);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.StylistName.GetHashCode();
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM stylists;";
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
