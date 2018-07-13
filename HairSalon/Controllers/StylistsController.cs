0a69cbbef5fa6ee92fa3f8cc21411af3e532bfad
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-13 11:59:40 -0700

    begin writing controller for finding all clients by stylist. currently only able to access stylist model. going on lunch

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -16,5 +16,30 @@ namespace HairSalon.Controllers
             allStylists = Stylist.GetAll();
             return View(allStylists);
         }
+
+        [HttpPost("/stylists")]
+        public ActionResult SaveStylist(int id, string name)
+        {
+            Stylist newStylist = new Stylist(id, name);
+            newStylist.Save();
+            return RedirectToAction("Details", new { id = newStylist.StylistId });
+        }
+
+        [HttpGet("/stylists/{id}")]
+        public ActionResult Details(int id)
+        {
+            Stylist currentStylist = Stylist.Find(id);
+            //List<Client> allStylistClients = new List<Client> { };
+            //allStylistClients = Stylist.GetClientsByStylist(id);
+            return View(currentStylist);
+        }
+
+        [HttpPost("/stylists/{id}")]
+        public ActionResult GetClients(int id)
+        {
+            List<Client> allStylistClients = new List<Client> { };
+            allStylistClients = Stylist.GetClientsByStylist(id);
+            return View("Details", allStylistClients)
+        }
     }
 }
