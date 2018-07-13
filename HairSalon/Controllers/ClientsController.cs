cdc809737a0a4edfd49ab914d4ff14085964e626
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-13 11:16:19 -0700

    add view and controller for viewing all clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
     public class ClientsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        [HttpGet("/clients")]
+        public ActionResult Index()
         {
-            return View();
+            List<Client> allClients = new List<Client> { };
+            allClients = Client.GetAll();
+            return View(allClients);
         }
     }
 }
