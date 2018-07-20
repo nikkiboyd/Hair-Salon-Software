8bbcd9c54dc28a5bd7b634db5e3603e051ab73e9
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-20 09:33:49 -0700

    create join table for stylists_specialties and begin writing specialty class

---
+++ HairSalon/Models/Specialty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace HairSalon.Models
+{
+    public class Specialty
+    {
+        public int SpecialtyId { get; set; }
+        public string SpecialtyType { get; set; }
+
+        public Specialty(string specialtyType, int specialtyId = 0)
+        {
+            SpecialtyType = specialtyType;
+            SpecialtyId = specialtyId;
+        }
+
+        public override bool Equals(System.Object otherSpecialty)
+        {
+            if (!(otherSpecialty is Specialty))
+            {
+                return false;
+            }
+            else
+            {
+                Specialty newSpecialty = (Specialty)otherSpecialty;
+                bool specialtyIdEquality = (this.SpecialtyId == newSpecialty.SpecialtyId);
+                bool specialtyTypeEquality = (this.SpecialtyType == newSpecialty.SpecialtyType);
+
+                return (specialtyIdEquality && specialtyTypeEquality);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.SpecialtyType.GetHashCode();
+        }
+
+    }
+}
